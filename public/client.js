0a551263a2273a2857943b0bbdca126cd26da964
mikemsrk <min2033@gmail.com>
2015-02-07 11:00:36 -0800

    Initiated repo

---
+++ public/client.js
@@ -0,0 +1,26 @@
+$(function(){
+
+	var socket = io.connect('http://localhost:8000');
+	
+	var name = '';
+	while(name === ''){
+		name = prompt('Name please');	
+	}
+	socket.emit('join',name);	
+	
+	socket.on('loadMessages',function(messages){
+		$('.viewer').find('p').remove();
+		messages.forEach(function(message){
+		$('.viewer').prepend('<p>' + message.name + ': ' + message.message + '</p>');
+		});
+
+	});
+	$('#chat').on('submit',function(e){
+		e.preventDefault();
+		var field = $('input[type="text"]');
+		var msg = field.val();
+		var obj = {user:name,message:msg};
+		socket.emit('userMsg',obj);
+		field.val('');
+	});
+});
